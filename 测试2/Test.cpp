59ea9783f60b087a7bcccfe271300295b2aa4f16
xiaowenad <1773879269@qq.com>
2023-09-24 11:09:00 +0800

    第二次提交

--- 测试2/Test.cpp
+++ 测试2/Test.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout<<"Hello heyuankai";
+    cout << "Hello World!" << endl;
     return 0;
 }
