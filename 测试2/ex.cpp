9fe3d2ae271b5c59908176549a5637d235e8ea69
xiaowenad <1773879269@qq.com>
2023-09-24 12:09:11 +0800

    加入一个求和函数add(int,int)

---
+++ 测试2/ex.cpp
@@ -0,0 +1,15 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << "a + b = " << add(a, b) << endl;
+	return 0;
+}
